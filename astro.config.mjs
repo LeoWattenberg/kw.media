c18a0251f23b4200afd4a8dcb84df50ad7edb6a9
Leo Wattenberg <48789553+LeoWattenberg@users.noreply.github.com>
2026-06-19 13:13:09 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,48 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 const site = process.env.ASTRO_SITE ?? 'https://kw.media';
 const base = process.env.ASTRO_BASE ?? '/';
 
 // https://astro.build/config
 export default defineConfig({
-	site,
-	base,
-	redirects: {
-		'/de/': '/',
+  site,
+  base,
 
-		'/ads/': '/en/ads/',
-		'/b2b/': '/de/b2b/',
-		'/creator/': '/de/creator/',
-		'/imprint-service/': '/en/imprint-service/',
-		'/live/': '/de/live/',
-		'/vtuber/': '/en/vtuber/',
-		'/werbung/': '/de/werbung/',
+  redirects: {
+      '/de/': '/',
 
-		'/new/ads/': '/en/ads/',
-		'/new/creator/': '/en/creator/',
-		'/new/': '/en/',
+      '/ads/': '/en/ads/',
+      '/b2b/': '/de/b2b/',
+      '/creator/': '/de/creator/',
+      '/imprint-service/': '/en/imprint-service/',
+      '/live/': '/de/live/',
+      '/vtuber/': '/en/vtuber/',
+      '/werbung/': '/de/werbung/',
 
-		'/blog/': '/en/youtube-tips/',
-		'/category/': '/en/youtube-tips/',
-		'/category/blog/': '/en/youtube-tips/',
-		'/category/uncategorized/': '/en/youtube-tips/',
-		'/category/uncategorized-de/': '/de/youtube-tipps/',
-		'/category/youtube-tips-de/': '/de/youtube-tipps/',
-		'/category/youtube-tips-en/': '/en/youtube-tips/',
-		'/category/youtube-tipps-de/': '/de/youtube-tipps/',
-		'/youtube-tips-en/': '/en/youtube-tips/',
-		'/youtube-tipps-de/': '/de/youtube-tipps/',
+      '/new/ads/': '/en/ads/',
+      '/new/creator/': '/en/creator/',
+      '/new/': '/en/',
 
-		'/author/koytekconsulting/': '/en/creator/',
-		'/author/leo/': '/en/vtuber/',
-		'/author/': '/en/creator/',
-		'/creatorguides/': '/en/youtube-tips/',
-		'/youtube/': 'https://www.youtube.com/channel/UCGu6U-UNczXKxShRiJj6kXQ',
+      '/blog/': '/en/youtube-tips/',
+      '/category/': '/en/youtube-tips/',
+      '/category/blog/': '/en/youtube-tips/',
+      '/category/uncategorized/': '/en/youtube-tips/',
+      '/category/uncategorized-de/': '/de/youtube-tipps/',
+      '/category/youtube-tips-de/': '/de/youtube-tipps/',
+      '/category/youtube-tips-en/': '/en/youtube-tips/',
+      '/category/youtube-tipps-de/': '/de/youtube-tipps/',
+      '/youtube-tips-en/': '/en/youtube-tips/',
+      '/youtube-tipps-de/': '/de/youtube-tipps/',
+
+      '/author/koytekconsulting/': '/en/creator/',
+      '/author/leo/': '/en/vtuber/',
+      '/author/': '/en/creator/',
+      '/creatorguides/': '/en/youtube-tips/',
+      '/youtube/': 'https://www.youtube.com/channel/UCGu6U-UNczXKxShRiJj6kXQ',
 	},
+
+  integrations: [sitemap()],
 });
