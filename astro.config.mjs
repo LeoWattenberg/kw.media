8f9d103fb756f2d93b82ed64592a0a74a81d9201
Leo Wattenberg <48789553+LeoWattenberg@users.noreply.github.com>
2026-06-19 11:29:57 +0200

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,36 @@ const base = process.env.ASTRO_BASE ?? '/';
 export default defineConfig({
 	site,
 	base,
+	redirects: {
+		'/de/': '/',
+
+		'/ads/': '/en/ads/',
+		'/b2b/': '/de/b2b/',
+		'/creator/': '/de/creator/',
+		'/imprint-service/': '/en/imprint-service/',
+		'/live/': '/de/live/',
+		'/vtuber/': '/en/vtuber/',
+		'/werbung/': '/de/werbung/',
+
+		'/new/ads/': '/en/ads/',
+		'/new/creator/': '/en/creator/',
+		'/new/': '/en/',
+
+		'/blog/': '/en/youtube-tips/',
+		'/category/': '/en/youtube-tips/',
+		'/category/blog/': '/en/youtube-tips/',
+		'/category/uncategorized/': '/en/youtube-tips/',
+		'/category/uncategorized-de/': '/de/youtube-tipps/',
+		'/category/youtube-tips-de/': '/de/youtube-tipps/',
+		'/category/youtube-tips-en/': '/en/youtube-tips/',
+		'/category/youtube-tipps-de/': '/de/youtube-tipps/',
+		'/youtube-tips-en/': '/en/youtube-tips/',
+		'/youtube-tipps-de/': '/de/youtube-tipps/',
+
+		'/author/koytekconsulting/': '/en/creator/',
+		'/author/leo/': '/en/vtuber/',
+		'/author/': '/en/creator/',
+		'/creatorguides/': '/en/youtube-tips/',
+		'/youtube/': 'https://www.youtube.com/channel/UCGu6U-UNczXKxShRiJj6kXQ',
+	},
 });
